2bc8aa7e1ab9f0fca2ef64bf1a0cc9786195f327
Gabriel Chulim <gabriel.chulim@revenatium.com>
2023-06-19 07:46:32 -0500

    JUnit 5 [#39 Initializing Properties with @BeforeEach]

---
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -0,0 +1,26 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GreetingTest {
+
+    Greeting greeting;
+    @BeforeEach
+    void setUp() {
+        System.out.println("In before Each...");
+        greeting = new Greeting();
+    }
+
+    @Test
+    void helloWorld() {
+        System.out.println(greeting.helloWorld());
+    }
+
+    @Test
+    void testHelloWorld() {
+        System.out.println(greeting.helloWorld("Gabriel"));
+    }
+}
